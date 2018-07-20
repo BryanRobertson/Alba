51c170f031e22591dbedfcc2839243e581234af6
Bryan Robertson <bryanericrobertson+github@gmail.com>
2018-07-19 23:05:43 -0400

    More work on resource system

--- Source/Engine/Core/Core_ResourceHandle.hpp
+++ Source/Engine/Core/Core_ResourceHandle.hpp
@@ -22,20 +22,27 @@ namespace Alba
 				typedef TResourceType				ResourceType;
 				typedef TResourceRepository			RepositoryType;
 
+				typedef ResourceHandle<typename TResourceType, typename TResourceRepository> ThisType;
+
 				//=================================================================================
 				// Public Constructors
 				//=================================================================================
 				ResourceHandle()
 					: myResourceId(IdType::InvalidId)
+					, myRepository(nullptr)
 				{
 
 				}
 				
-				explicit ResourceHandle(IdType anId)
+				explicit ResourceHandle(IdType anId, RepositoryType& aRepository)
 					: myResourceId(anId)
+					, myRepository(&aRepository)
 				{
 
 				}
+
+				explicit ResourceHandle(const ThisType& aCopyFrom) = default;
+				explicit ResourceHandle(ThisType&& aMoveFrom) = default;
 
 				//=================================================================================
 				// Public Methods
@@ -45,11 +52,59 @@ namespace Alba
 					return myResourceId;
 				}
 
+				const ResourceType* Lock() const
+				{
+					return myRepository->GetResourcePtr(myResourceId);
+				}
+
+				ResourceType* LockMutable()
+				{
+					return myRepository->GetResourcePtrMutable(myResourceId);
+				}
+
+				void Unlock()
+				{
+					// Does nothing for now
+				}
+
+				bool operator== (const ThisType& aHandle) const
+				{
+					return myResourceId == aHandle.myResourceId 
+						&& myRepository == aHandle.myRepository;
+				}
+
+				bool operator< (const ThisType& aHandle) const
+				{
+					return myResourceId < aHandle.myResourceId;
+				}
+
+				bool operator<= (const ThisType& aHandle) const
+				{
+					return myResourceId <= aHandle.myResourceId;
+				}
+
+				bool operator> (const ThisType& aHandle) const
+				{
+					return myResourceId < aHandle.myResourceId;
+				}
+
+				bool operator>= (const ThisType& aHandle) const
+				{
+					return myResourceId >= aHandle.myResourceId;
+				}
+
+				ThisType& operator= (const ThisType& aHandle)
+				{
+					myResourceId = aHandle.myResourceId;
+					myRepository = aHandle.myRepository;
+				}
+
 			private:
 
 				//=================================================================================
 				// Private Data
 				//=================================================================================
+				TResourceRepository* myRepository;
 				IdType				 myResourceId;
 		};
 	}
--- Source/Engine/Core/Core_ResourceRepository.hpp
+++ Source/Engine/Core/Core_ResourceRepository.hpp
@@ -11,6 +11,22 @@ namespace Alba
 {
 	namespace Core
 	{
+		union ResourceIdInternal
+		{
+			// Note: Size must add up to 64 bits
+			struct Fields
+			{
+				uint32	myIndex		: 32;
+				uint32  myNameHash	: 32;
+			};
+
+			Fields myFields;
+			uint64 myValue;
+
+			// Ensure fields fit inside myValue
+			static_assert(sizeof(myFields) == sizeof(myValue));
+		};
+
 		//-----------------------------------------------------------------------------------------
 		// Name	:	ResourceRepository
 		// Desc	:	Provides reference-counted storage/access to a specific resource type
@@ -18,31 +34,48 @@ namespace Alba
 		template <typename TDerived, typename TResourceType>
 		class ResourceRepository
 		{
+			public:
+
+				//=================================================================================
+				// Public Types
+				//=================================================================================
+				typedef ResourceHandle<TResourceType, ResourceRepository<TDerived, TResourceType> > Handle;
+				friend class ResourceHandle<TResourceType, ResourceRepository<TDerived, TResourceType> >;
+
+				//=================================================================================
+				// Public Constructors/Destructors
+				//=================================================================================
+				~ResourceRepository();
+
+				//=================================================================================
+				// Public Methods
+				//=================================================================================
+				inline Handle				GetResource(NoCaseStringHash32 aResourceNameId) const;
+				inline bool					HasResource(NoCaseStringHash32 aResourceNameId) const;
+
 			protected:
 
-				typedef ResourceHandle<TResourceType, ResourceRepository<TDerived, TResourceType> > Handle;
-				typedef ResourceReference<TResourceType, ResourceRepository<TDerived, TResourceType> > Reference;
-				
 				//=================================================================================
 				// Protected Constructors
 				//=================================================================================
 				ResourceRepository(uint32 aReserveSize);
-				~ResourceRepository();
 
 				//=================================================================================
-				// Protected Methods
+				// Protected Constructors
 				//=================================================================================
-				inline Handle				GetHandle(NoCaseStringHash32 aResourceNameId) const;
-				inline const TResourceType*	GetResource(Handle aHandle) const;
-				inline TResourceType*		GetResourceMutable(Handle aHandle);
-
-				inline bool					HasResource(NoCaseStringHash32 aResourceNameId) const;
-
-				inline Handle				Create(NoCaseStringHash32 aResourceNameId);
-				inline void					Destroy(NoCaseStringHash32 aResourceNameId);
-				inline void					Destroy(Handle aHandle);
-
+				inline Handle				CreateResource(NoCaseStringHash32 aResourceNameId);
+				inline void					DestroyResource(NoCaseStringHash32 aResourceNameId);
+				inline void					DestroyResource(Handle aHandle);
+			
 			private:
+
+				//=================================================================================
+				// Private Methods
+				//=================================================================================
+				inline const TResourceType*	GetResourcePtr(Handle aHandle) const;
+				inline TResourceType*		GetResourcePtrMutable(Handle aHandle);
+
+				inline void					DestroyResourceInternal(size_t anIndex);
 
 				//=================================================================================
 				// Private Data
@@ -51,6 +84,9 @@ namespace Alba
 
 				VectorMap<NoCaseStringHash32, uint32>	myNameIdHashToIndex;
 				BitVector<uint64>						myFreeIndices;
+
+				Mutex									myModifyFreeIndicesMutex;
+				ReadWriteMutex							myModifyNameIdHashToIndex;
 		};
 
 		//-----------------------------------------------------------------------------------------
@@ -67,14 +103,17 @@ namespace Alba
 		template <typename TDerived, typename TResourceType>
 		/*inline*/ ResourceRepository<TDerived, TResourceType>::~ResourceRepository()
 		{
-			
+			myResources.clear();
+			myFreeIndices.clear();
 		}
 
 		//-----------------------------------------------------------------------------------------
 		//-----------------------------------------------------------------------------------------
 		template <typename TDerived, typename TResourceType>
-		ResourceRepository<TDerived, TResourceType>::Handle ResourceRepository<TDerived, TResourceType>::GetHandle(NoCaseStringHash32 aResourceNameId) const
+		ResourceRepository<TDerived, TResourceType>::Handle ResourceRepository<TDerived, TResourceType>::GetResource(NoCaseStringHash32 aResourceNameId) const
 		{
+			Core::ScopedReaderMutexLock lock(myModifyNameIdHashToIndex);
+
 			auto itr = myNameIdHashToIndex.find(aResourceNameId);
 			if (itr != myNameIdHashToIndex.end())
 			{
@@ -90,29 +129,123 @@ namespace Alba
 		//-----------------------------------------------------------------------------------------
 		//-----------------------------------------------------------------------------------------
 		template <typename TDerived, typename TResourceType>
-		const TResourceType* ResourceRepository<TDerived, TResourceType>::GetResource(ResourceRepository<TDerived, TResourceType>::Handle aResourceHandle) const
+		/*inline*/ bool ResourceRepository<TDerived, TResourceType>::HasResource(NoCaseStringHash32 aResourceNameId) const
 		{
-			//const TResourceType* resource = myResources[itr->second];
-			//resource->GetId();
-			return nullptr;
-		}
+			Core::ScopedReaderMutexLock lock(myModifyNameIdHashToIndex);
 
-		//-----------------------------------------------------------------------------------------
-		//-----------------------------------------------------------------------------------------
-		template <typename TDerived, typename TResourceType>
-		TResourceType* ResourceRepository<TDerived, TResourceType>::GetResourceMutable(ResourceRepository<TDerived, TResourceType>::Handle aResourceHandle)
-		{
-			//const TResourceType* resource = myResources[itr->second];
-			//resource->GetId();
-			return nullptr;
-		}
-
-		//-----------------------------------------------------------------------------------------
-		//-----------------------------------------------------------------------------------------
-		template <typename TDerived, typename TResourceType>
-		bool ResourceRepository<TDerived, TResourceType>::HasResource(NoCaseStringHash32 aResourceNameId) const
-		{
 			return myNameIdHashToIndex.find(aResourceNameId) != myResources.end();
+		}
+
+		//-----------------------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------------
+		template <typename TDerived, typename TResourceType>
+		/*inline*/ ResourceRepository<TDerived, TResourceType>::Handle ResourceRepository<TDerived, TResourceType>::CreateResource(NoCaseStringHash32 aResourceNameId)
+		{
+			uint64 index = 0;
+
+			{
+				Core::ScopedMutexLock lock(myModifyFreeIndicesMutex);
+
+				// Find first free index
+				uint64 baseIndex = 0;
+
+				for (uint64 entry : myFreeIndices.get_container())
+				{
+					index = index & (index - 1);
+
+					if (index != 0)
+					{
+						index += baseIndex;			
+						break;
+					}
+
+					baseIndex += (sizeof(entry) * 8);
+				}
+
+				// If index is invalid then bail
+				if (index >= myFreeIndices.size() || !myFreeIndices[index])
+				{
+					return Handle();
+				}
+
+				myFreeIndices[index] = false;
+			}
+			
+			// Generate ID
+			ResourceIdInternal idFields;
+			idFields.myFields.myIndex = index;
+			idFields.myFields.myNameHash = aResourceNameId.GetHash();
+
+			ResourceIdInternal idValue;
+			std::memcpy(&idValue, *idFields, sizeof(idValue));
+
+			const ResourceId<TResourceType> id(idValue.myValue);
+			
+			// Set resource
+			myResources[index] = TResourceType(id);
+
+			// Set name lookup
+			{
+				Core::ScopedWriterMutexLock lock(myModifyNameIdHashToIndex);
+				myNameIdHashToIndex[aResourceNameId] = index;
+			}
+
+			const Handle resourceHandle(aResourceNameId, id);
+
+		}
+
+		//-----------------------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------------
+		template <typename TDerived, typename TResourceType>
+		/*inline*/ void ResourceRepository<TDerived, TResourceType>::DestroyResource(NoCaseStringHash32 aResourceNameId)
+		{
+			const Handle resourceHandle = GetResource(aResourceNameId);
+
+			auto itr = myNameIdHashToIndex.find(aResourceNameId);
+			if (itr != myNameIdHashToIndex.end())
+			{
+				DestroyResourceInternal(itr->second);
+			}
+		}
+
+		//-----------------------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------------
+		template <typename TDerived, typename TResourceType>
+		/*inline*/ void ResourceRepository<TDerived, TResourceType>::DestroyResource(Handle aHandle)
+		{
+			const ResourceId<TResourceType> id = aHandle.GetId();
+			const uint64 idValue = id.GetValue();
+
+			ResourceIdInternal idFields;
+			std::memcpy(&idFields, &idValue, sizeof(idFields));
+		}
+
+		//-----------------------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------------
+		template <typename TDerived, typename TResourceType>
+		/*inline*/ void	ResourceRepository<TDerived, TResourceType>::DestroyResourceInternal(size_t anIndex)
+		{
+
+		}
+
+		//-----------------------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------------
+		template <typename TDerived, typename TResourceType>
+		const TResourceType* ResourceRepository<TDerived, TResourceType>::GetResourcePtr(ResourceRepository<TDerived, TResourceType>::Handle aResourceHandle) const
+		{
+			//const TResourceType* resource = myResources[itr->second];
+			//resource->GetId();
+			return nullptr;
+		}
+
+		//-----------------------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------------
+		template <typename TDerived, typename TResourceType>
+		TResourceType* ResourceRepository<TDerived, TResourceType>::GetResourcePtrMutable(ResourceRepository<TDerived, TResourceType>::Handle aResourceHandle)
+		{
+			//const TResourceType* resource = myResources[itr->second];
+			//resource->GetId();
+			return nullptr;
 		}
 	}
 }
