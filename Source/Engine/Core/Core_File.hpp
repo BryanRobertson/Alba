4600e8332b60678414490676240a0968fc465eb0
Bryan Robertson <bryanericrobertson+github@gmail.com>
2018-09-24 21:56:54 -0400

    File implementation

--- Source/Engine/Core/Core_File.hpp
+++ Source/Engine/Core/Core_File.hpp
@@ -5,6 +5,9 @@
 #include "Core_Array.hpp"
 #include "Core_TypeTraits.hpp"
 
+#include <cstdio>
+#include <cstdlib>
+
 namespace Alba
 {
 	namespace Core
@@ -58,7 +61,16 @@ namespace Alba
 				//=================================================================================
 				// Public Static Methods
 				//=================================================================================
-				File		Open(StringView aFileName, FileModeSet someFileModes);
+				File Open(StringView aFileName, FileModeSet someFileModes)
+				{
+					return File(aFileName, someFileModes);
+				}
+
+				//=================================================================================
+				// Public Constructors/Destructors
+				//=================================================================================
+				File(const File&) = delete;
+				File(File&&) = default;
 
 				//=================================================================================
 				// Public Methods
@@ -67,41 +79,137 @@ namespace Alba
 				//---------------------------------------------------------------------------------
 				// Accessors
 				//---------------------------------------------------------------------------------
-				size_t		GetSize() const;
-				size_t		GetPosition() const;
+				ALBA_FORCEINLINE size_t	GetSize() const
+				{
+					return myFileSize;
+				}
+
+				ALBA_FORCEINLINE size_t	GetPosition() const
+				{
+					return std::ftell(myFile);
+				}
+
+				ALBA_FORCEINLINE FileModeSet GetOpenMode() const
+				{
+					return myFileModes;
+				}
+
+				ALBA_FORCEINLINE bool IsValid() const
+				{
+					return myFile != nullptr;
+				}
+
+				ALBA_FORCEINLINE void Close()
+				{
+					std::fclose(myFile);
+					myFile = nullptr;
+				}
 
 				//---------------------------------------------------------------------------------
 				// Read
 				//---------------------------------------------------------------------------------
 				template <typename TDataType>
-					size_t Read(TDataType* aDataOut, size_t aCount)
+				ALBA_FORCEINLINE size_t Read(TDataType* aDataOut, size_t aCount)
 				{
 					return Read(aDataOut, sizeof(TDataType), anElementCount);
 				}
 
 				template <typename TDataType, class=enable_if_t<is_standard_layout_v<TDataType> > >
-					size_t Read(TDataType& aDataOut)
+				ALBA_FORCEINLINE size_t Read(TDataType& aDataOut)
 				{
 					return Read(&aDataOut, 1);
+				}
+
+				template <typename TCharType, size_t TCount, OverflowBehavior TOverflow, typename TAllocator>
+				ALBA_FORCEINLINE size_t Read(FixedBasicString<TCharType, TCount, TOverflow, TAllocator>& aDataOut, size_t aCount)
+				{
+					aDataOut.reserve(aCount);
+					return Read(&aDataOut[0], sizeof(TCharType), aCount);
+				}
+
+				template <typename TCharType, size_t TCount, OverflowBehavior TOverflow, typename TAllocator>
+				ALBA_FORCEINLINE size_t Read(FixedBasicString<TCharType, TCount, TOverflow, TAllocator>& aDataOut, size_t aCount)
+				{
+					aDataOut.reserve(aCount);
+					return Read(&aDataOut[0], sizeof(TCharType), aCount);
 				}
 
 				//---------------------------------------------------------------------------------
 				// Write
 				//---------------------------------------------------------------------------------
+				template <typename TDataType>
+				ALBA_FORCEINLINE size_t Write(const TDataType* aDataOut, size_t aCount)
+				{
+					return Write(aDataOut, aCount);
+				}
 
+				template <typename TDataType, class = enable_if_t<is_standard_layout_v<TDataType> > >
+				ALBA_FORCEINLINE size_t Write(const TDataType& aDataOut)
+				{
+					return Write(&aDataOut, 1);
+				}
+
+				template <typename TCharType, size_t TCount, OverflowBehavior TOverflow, typename TAllocator>
+				ALBA_FORCEINLINE size_t Write(FixedBasicString<TCharType, TCount, TOverflow, TAllocator>& aDataOut, size_t aCount=aDataOut.size())
+				{
+					return Write(&aDataOut[0], sizeof(TCharType), aCount);
+				}
+
+				template <typename TCharType, typename TAllocator>
+				ALBA_FORCEINLINE size_t Write(BasicString<TCharType, TAllocator>& aDataOut, size_t aCount=aDataOut.size())
+				{
+					return Write(&aDataOut[0], sizeof(TCharType), aCount);
+				}
 
 			private:
 
 				//=================================================================================
+				// Private Constructors
+				//=================================================================================
+				File(StringView aFileName, FileModeSet someFileModes)
+					: myFileName(aFileName)
+					, myFileModes(someFileModes)
+				{
+					FixedString<8> mode;
+					if (someFileModes.Contains(FileMode::Read))
+					{
+						mode.append("r");
+					}
+
+					if (someFileModes.Contains(FileMode::Write))
+					{
+						mode.append("w");
+					}
+
+					myFile = std::fopen(aFileName.data(), mode.data());
+					if (myFile)
+					{
+						std::fseek(myFile, 0, SEEK_END);
+						myFileSize = std::ftell(myFile);
+						std::fseek(myFile, 0, SEEK_SET);
+					}
+				}
+
+				//=================================================================================
 				// Private Methods
 				//=================================================================================
-				size_t Read(void* aDataOut, size_t anElementSize, size_t anElementCount);
+				size_t Read(void* aDataOut, size_t anElementSize, size_t anElementCount)
+				{
+					return std::fread(aDataOut, anElementSize, anElementCount, myFile);
+				}
+
+				size_t Write(void* aDataOut, size_t anElementSize, size_t anElementCount)
+				{
+					return std::fwrite(aDataOut, anElementSize, anElementCount, myFile);
+				}
 
 				//=================================================================================
 				// Private Data
 				//=================================================================================
 				String			myFileName;
 				FileModeSet		myFileModes;
+				std::FILE*		myFile;
+				size_t			myFileSize;
 		};
 	}
 }
